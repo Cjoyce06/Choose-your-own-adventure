2d43aa239c9ef49d09f10c14c8cbb1ce4aeb17dd
Colin Joyce <Cjoyce1@bsd220.org>
2022-12-12 16:03:42 -0600

    Initial Commit

---
+++ Choose your own adventure/Choose_your_own_adventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Choose_your_own_adventureApp.swift
+//  Choose your own adventure
+//
+//  Created by Colin Joyce on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Choose_your_own_adventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
