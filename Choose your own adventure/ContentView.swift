6522141e88b8b0a7464492fd04434f78f1f025d9
Colin Joyce <Cjoyce1@bsd220.org>
2022-12-12 16:21:09 -0600

    Added title and tab view

--- Choose your own adventure/ContentView.swift
+++ Choose your own adventure/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView {
+            VStack {
+                HStack {
+                    Text("Choose your own adventure").fixedSize(horizontal: true, vertical: false)
+                        .font(.title)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                }
+                Text("Intro")
+            }
+            Button("Choice 1", action: {})
+                .padding()
+        } .tabViewStyle(.page)
     }
 }
 
